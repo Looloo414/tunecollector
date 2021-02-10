cda5e5e2528c046d1e07a2919fd095e3d8b00ded
Looloo414 <alexafaulkison@gmail.com>
2021-02-10 02:42:10 -0700

    message

---
+++ main_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def tunes_index(request):
+  return render(request, 'tunes/index.html', { 'tunes': tunes })
